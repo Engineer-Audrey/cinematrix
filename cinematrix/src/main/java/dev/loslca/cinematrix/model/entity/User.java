d915db966b1cee50ce179de0ae592f3eba5f8b45
Ing Estiven <estuvenrojas@gmail.com>
2026-08-26 17:11:50 -0500

    repository y long

--- cinematrix/src/main/java/dev/loslca/cinematrix/model/entity/User.java
+++ cinematrix/src/main/java/dev/loslca/cinematrix/model/entity/User.java
@@ -11,7 +11,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private Integer userId;
+    private Long userId;
 
     @Column(name = "first_name", nullable = false, length = 100)
     private String firstName;
@@ -46,8 +46,8 @@ public class User {
 
     public User() {}
 
-    public Integer getUserId() { return userId; }
-    public void setUserId(Integer userId) { this.userId = userId; }
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
 
     public String getFirstName() { return firstName; }
     public void setFirstName(String firstName) { this.firstName = firstName; }
