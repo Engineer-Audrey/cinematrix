3642da1bcc4181e835ea388b79aa712aa90c1fd4
sebas <sebasleonivi@gmail.com>
2026-08-26 17:15:04 -0500

    Repository de Location y Castin

--- cinematrix/src/main/java/dev/loslca/cinematrix/repository/LocationRepository.java
+++ cinematrix/src/main/java/dev/loslca/cinematrix/repository/LocationRepository.java
@@ -1,6 +1,6 @@
 package dev.loslca.cinematrix.repository;
 
-import dev.loslca.cinematrix.model.entity.Casting;
+import dev.loslca.cinematrix.model.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocationRepository extends JpaRepository<Casting, Long> {
